949b5f2f9805fa1ff1efbfa0ea440323d7305156
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-06-21 19:24:07 +0300

    input move

---
+++ input.js
@@ -0,0 +1,21 @@
+(function () {
+    const input = new Object;
+    function onChange(event) {
+        var reader = new FileReader();
+        reader.onload = onReaderLoad;
+        if (event.target.files[1]) {
+            event.target.files[0] = event.target.files[1];
+            event.target.files[1].remove();
+        }
+        reader.readAsText(event.target.files[0]);
+        input.filename = event.target.files[0].name;
+    }
+
+    function onReaderLoad(event) {
+        input = JSON.parse(event.target.result);
+        convert(input);
+    }
+
+    document.getElementById('file').addEventListener('change', onChange);
+
+}())
